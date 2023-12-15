5367478c8574890a08502aa811329de4a461dcda
Sarinzhan <sarinzhanakazbekov@gmail.com>
2023-12-15 19:33:04 +0600

    add lab2 and lab3 packages, some changes in lab1

--- src/main/java/lab1/Calculator.java
+++ src/main/java/lab1/Calculator.java
@@ -1,16 +1,26 @@
+package lab1;
+
 import java.util.Scanner;
 
 public class Calculator {
     private static final Scanner sc = new Scanner(System.in);
     static public void calculateCircumference() {//длина окружности
         System.out.print("Введите радиус для вычисления длины окружности");
-        int radius = sc.nextInt();
-        System.out.println(2 * Math.PI * radius);
+        int radius = 0;
+        while(!sc.hasNextInt()){
+            System.out.println("Введите целое число");
+            sc.next();
+        }
+        radius = sc.nextInt();
+        System.out.printf("Длина окружности: %.0f \n", 2 * Math.PI * radius);
     }
-
     static public void calculateArea() {//площадь круга
         System.out.print("Введите радиус для вычисления площади круга");
+        while(!sc.hasNextInt()){
+            System.out.println("Введите целое число");
+            sc.next();
+        }
         int radius = sc.nextInt();
-        System.out.println(Math.PI * Math.pow(radius, 2));
+        System.out.printf("Площадь круга: %.0f \n",Math.PI * Math.pow(radius, 2));
     }
 }
